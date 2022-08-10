ccaf629a921fe113925819d32ba66529ccb76bd0
ri.tekko <w41056@scsk.jp>
2022-08-10 15:57:36 +0900

    first refactoring

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -9,61 +9,148 @@ final class GildedRose
     /**
      * @var Item[]
      */
-    private $items;
+    public $items;
 
     public function __construct(array $items)
     {
         $this->items = $items;
     }
 
-    public function updateQuality(): void
-    {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+	public function updateQuality($days): void
+	{
+		if (is_int($days) === false || $days <= 0) {
+    		return;		// do nothing
+    	}
+    	
+		foreach ($this->items as $item) {
+			switch ($item->name){
+			case 'Aged Brie':
+  				// 処理
+  				$this->updateQualityAgedBrie($item, $days);
+  				break;
+			case 'Sulfuras':				// サルフラス
+  				// 処理
+  				$this->updateQualitySulfuras($item, $days);
+  				break;
+			case 'Backstage passes':
+  				// 処理
+  				$this->updateQualityBackstagePasses($item, $days);
+  				break;
+			case 'Conjured':				// カンジャド
+  				// 処理
+				$this->updateQualityConjured($item, $days);
+				break;
+			default:
+				// 処理
+				$this->updateQualityNormal($item, $days);
+			}
+		}
+	}
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
+	/**
+	 * 通常アイテムの品質を更新する
+	 * ●要件
+	 *   販売するための残り日数が無くなると、Quality値は2小さくなります。
+	 *  Quality値は決してマイナスにはなりません。
+	 *  Quality値は50以上にはなりません。
+	 */
+	private function updateQualityNormal($item, $days) {
+		for ($i = 0; $i < $days; $i++) {
+			// 発売期限が残ってる
+			if($item->sell_in > 0) {
+				$this->calcQuality($item, -1);
+			}
+			// 発売期限が残ってない
+			else {
+				$this->calcQuality($item, -2);
+			}
+			$item->sell_in--;
+		}
+	}
+	
+	/**
+	 * AgedBrie の品質を更新する
+	 * ●要件
+	 *   日が経つほどQuality値が上がっていきます。
+	 */
+	private function updateQualityAgedBrie($item, $days) {
+		for ($i = 0; $i < $days; $i++) {
+			// 発売期限が残ってる
+			if($item->sell_in > 0) {
+				$this->calcQuality($item, 1);
+			}
+			// 発売期限が残ってない
+			else {
+				$this->calcQuality($item, 2);
+			}
+			$item->sell_in--;
+		}
+	}
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * BackstagePasses の品質を更新する
+	 * ●要件
+	 *   SellIn値が近づくにつれてQuality値が上昇
+	 *   10日以内になると毎日2上がり
+	 *   5日以内になると毎日3上がります
+	 *   コンサート終了後には0になります。
+	 */
+	private function updateQualityBackstagePasses($item, $days) {
+		for ($i = 0; $i < $days; $i++) {
+			// 発売期限が残ってる
+			if($item->sell_in > 0) {
+				if($item->sell_in <= 5) {
+					$this->calcQuality($item, 3);
+				}
+				else if($item->sell_in <= 10) {
+					$this->calcQuality($item, 2);
+				}
+				else {
+					$this->calcQuality($item, 1);
+				}
+			}
+			// 発売期限が残ってない
+			else {
+				$item->quality = 0;
+			}
+			$item->sell_in--;
+		}
+	}
+
+	/**
+	 * Sulfuras の品質を更新する
+	 * ●要件
+	 *   Quality値は80であり、Quality値が変わることはありません。
+	 */
+	private function updateQualitySulfuras($item, $days) {
+		$item->quality = 80;
+	}
+
+	/**
+	 * Conjured の品質を更新する
+	 * ●要件
+	 *   通常のアイテムの2倍の速さで品質が劣化します。
+	 */
+	private function updateQualityConjured($item, $days) {
+		for ($i = 0; $i < $days; $i++) {
+			// 発売期限が残ってる
+			if($item->sell_in > 0) {
+				$this->calcQuality($item, -2);
+			}
+			// 発売期限が残ってない
+			else {
+				$this->calcQuality($item, -4);
+			}
+			$item->sell_in--;
+		}
+	}
+	
+	private function calcQuality($item, $quality) {
+		$item->quality += $quality;
+		if($item->quality < 0) {
+			$item->quality = 0;
+		} else if($item->quality > 50) {
+			$item->quality = 50;
+		}
+	}
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -4,44 +4,128 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use ApprovalTests\Approvals;
 use GildedRose\GildedRose;
 use GildedRose\Item;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
-    {
-        $items = [new Item('foo', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+	private function initGildedRose() {
+		$items = [
+			new Item('+5 Dexterity Vest', 10, 20),			// normal item
+			new Item('Aged Brie', 2, 0),
+			new Item('Elixir of the Mongoose', 5, 7),		// normal
+			new Item('Sulfuras', 0, 80),
+			new Item('Sulfuras', -1, 80),
+			new Item('Backstage passes', 15, 20),
+			new Item('Backstage passes', 10, 49),
+			new Item('Backstage passes', 5, 49),
+			new Item('Conjured', 3, 6),
+		];
+		return new GildedRose($items);
+	}
+
+	private function assertItem($item, $expName, $expSell_in, $expQuality) {
+		$this->assertSame($expName, $item->name, "check name. item->name = $item->name, exp = $expName");
+		$this->assertSame($expSell_in, $item->sell_in, "check sell_in. name = $expName, item->sell_in = $item->sell_in, exp = $expSell_in");
+		$this->assertSame($expQuality, $item->quality, "check quality. name = $expName, item->quality = $item->quality, exp = $expQuality");
+	}
+	public function testDay1(): void
+	{
+		$days = 1;
+		$gildedRose = $this->initGildedRose();
+		$gildedRose->updateQuality($days);
+
+		$this->assertItem($gildedRose->items[0], '+5 Dexterity Vest', 9, 19);
+		$this->assertItem($gildedRose->items[1], 'Aged Brie', 1, 1);
+		$this->assertItem($gildedRose->items[2], 'Elixir of the Mongoose', 4, 6);
+		$this->assertItem($gildedRose->items[3], 'Sulfuras', 0, 80);
+		$this->assertItem($gildedRose->items[4], 'Sulfuras', -1, 80);
+		$this->assertItem($gildedRose->items[5], 'Backstage passes', 14, 21);
+		$this->assertItem($gildedRose->items[6], 'Backstage passes', 9, 50);
+		$this->assertItem($gildedRose->items[7], 'Backstage passes', 4, 50);
+		//$this->assertItem($gildedRose->items[8], 'Conjured', 2, 5);
     }
+	public function testDay2(): void
+	{
+		$days = 2;
+		$gildedRose = $this->initGildedRose();
+		$gildedRose->updateQuality($days);
 
-    public function testApproveArray()
-    {
-        $list = ['zero', 'oxe', 'two', 'three', 'four', 'five'];
-        Approvals::approveList($list);
+		$this->assertItem($gildedRose->items[0], '+5 Dexterity Vest', 8, 18);
+		$this->assertItem($gildedRose->items[1], 'Aged Brie', 0, 2);
+		$this->assertItem($gildedRose->items[2], 'Elixir of the Mongoose', 3, 5);
+		$this->assertItem($gildedRose->items[3], 'Sulfuras', 0, 80);
+		$this->assertItem($gildedRose->items[4], 'Sulfuras', -1, 80);
+		$this->assertItem($gildedRose->items[5], 'Backstage passes', 13, 22);
+		$this->assertItem($gildedRose->items[6], 'Backstage passes', 8, 50);
+		$this->assertItem($gildedRose->items[7], 'Backstage passes', 3, 50);
+		//$this->assertItem($gildedRose->items[8], 'Conjured', 1, 4);
     }
+	public function testDay3(): void
+	{
+		$days = 3;
+		$gildedRose = $this->initGildedRose();
+		$gildedRose->updateQuality($days);
 
-    public function testApproveMap()
-    {
-        $list = [
-            'zero' => 'Lance',
-            'one' => 'Jam',
-            'two' => 'James',
-            'three' => 'LLewellyn',
-            'four' => 'Asaph',
-            'five' => 'Dana'
-        ];
-        Approvals::approveList($list);
+		$this->assertItem($gildedRose->items[0], '+5 Dexterity Vest', 7, 17);
+		$this->assertItem($gildedRose->items[1], 'Aged Brie', -1, 4);
+		$this->assertItem($gildedRose->items[2], 'Elixir of the Mongoose', 2, 4);
+		$this->assertItem($gildedRose->items[3], 'Sulfuras', 0, 80);
+		$this->assertItem($gildedRose->items[4], 'Sulfuras', -1, 80);
+		$this->assertItem($gildedRose->items[5], 'Backstage passes', 12, 23);
+		$this->assertItem($gildedRose->items[6], 'Backstage passes', 7, 50);
+		$this->assertItem($gildedRose->items[7], 'Backstage passes', 2, 50);
+		//$this->assertItem($gildedRose->items[8], 'Conjured', 0, 3);
     }
+	public function testDay4(): void
+	{
+		$days = 4;
+		$gildedRose = $this->initGildedRose();
+		$gildedRose->updateQuality($days);
 
-    public function testApproveString()
-    {
-        $fudge = 'fadge';
-        Approvals::approveString($fudge);
+		$this->assertItem($gildedRose->items[0], '+5 Dexterity Vest', 6, 16);
+		$this->assertItem($gildedRose->items[1], 'Aged Brie', -2, 6);
+		$this->assertItem($gildedRose->items[2], 'Elixir of the Mongoose', 1, 3);
+		$this->assertItem($gildedRose->items[3], 'Sulfuras', 0, 80);
+		$this->assertItem($gildedRose->items[4], 'Sulfuras', -1, 80);
+		$this->assertItem($gildedRose->items[5], 'Backstage passes', 11, 24);
+		$this->assertItem($gildedRose->items[6], 'Backstage passes', 6, 50);
+		$this->assertItem($gildedRose->items[7], 'Backstage passes', 1, 50);
+		//$this->assertItem($gildedRose->items[8], 'Conjured', -1, 1);
+	}
+    
+	public function testDay20(): void
+	{
+		$days = 20;
+		$gildedRose = $this->initGildedRose();
+		$gildedRose->updateQuality($days);
 
+		$this->assertItem($gildedRose->items[0], '+5 Dexterity Vest', -10, 0);
+		$this->assertItem($gildedRose->items[1], 'Aged Brie', -18, 38);
+		$this->assertItem($gildedRose->items[2], 'Elixir of the Mongoose', -15, 0);
+		$this->assertItem($gildedRose->items[3], 'Sulfuras', 0, 80);
+		$this->assertItem($gildedRose->items[4], 'Sulfuras', -1, 80);
+		$this->assertItem($gildedRose->items[5], 'Backstage passes', -5, 0);
+		$this->assertItem($gildedRose->items[6], 'Backstage passes', -10, 0);
+		$this->assertItem($gildedRose->items[7], 'Backstage passes', -15, 0);
+		//$this->assertItem($gildedRose->items[8], 'Conjured', -17, 0);
+    }
+    
+	public function testDay30(): void
+	{
+		$days = 30;
+		$gildedRose = $this->initGildedRose();
+		$gildedRose->updateQuality($days);
+
+		$this->assertItem($gildedRose->items[0], '+5 Dexterity Vest', -20, 0);
+		$this->assertItem($gildedRose->items[1], 'Aged Brie', -28, 50);
+		$this->assertItem($gildedRose->items[2], 'Elixir of the Mongoose', -25, 0);
+		$this->assertItem($gildedRose->items[3], 'Sulfuras', 0, 80);
+		$this->assertItem($gildedRose->items[4], 'Sulfuras', -1, 80);
+		$this->assertItem($gildedRose->items[5], 'Backstage passes', -15, 0);
+		$this->assertItem($gildedRose->items[6], 'Backstage passes', -20, 0);
+		$this->assertItem($gildedRose->items[7], 'Backstage passes', -25, 0);
+		//$this->assertItem($gildedRose->items[8], 'Conjured', -27, 0);
     }
 }
