bc140151a39c8b5ea4cb506be00afef9116bd7ee
masaosaga <masao.saga@nex.nikkei.com>
2022-08-12 16:42:56 +0900

    normalを作成

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,51 +19,82 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
+            if ($item->name === 'Aged Brie'){
+                $this->AgedBrie();
+            } elseif ($item->name === 'Sulfuras, Hand of Ragnaros'){
+                $this->Sulfuras();
+            } elseif ($item->name === 'Backstage passes to a TAFKAL80ETC concert'){
+                $this->BackstagePasses();
             } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
+                $this->normal($item);
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
+            // if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            //     if ($item->quality > 0) {
+            //         if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            //             $item->quality = $item->quality - 1;
+            //         }
+            //     }
+            // } else {
+            //     if ($item->quality < 50) {
+            //         $item->quality = $item->quality + 1;
+            //         if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+            //             if ($item->sell_in < 11) {
+            //                 if ($item->quality < 50) {
+            //                     $item->quality = $item->quality + 1;
+            //                 }
+            //             }
+            //             if ($item->sell_in < 6) {
+            //                 if ($item->quality < 50) {
+            //                     $item->quality = $item->quality + 1;
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            // if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            //     $item->sell_in = $item->sell_in - 1;
+            // }
+
+            // if ($item->sell_in < 0) {
+            //     if ($item->name != 'Aged Brie') {
+            //         if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            //             if ($item->quality > 0) {
+            //                 if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            //                     $item->quality = $item->quality - 1;
+            //                 }
+            //             }
+            //         } else {
+            //             $item->quality = $item->quality - $item->quality;
+            //         }
+            //     } else {
+            //         if ($item->quality < 50) {
+            //             $item->quality = $item->quality + 1;
+            //         }
+            //     }
+            // }
         }
     }
+
+    private function AgedBrie(): void
+    {
+    }
+
+    private function Sulfuras(): void
+    {
+    }
+
+    private function BackstagePasses(): void
+    {
+    }
+
+    private function normal($item): void
+    {
+        if ($item->sell_in !== 0) {
+            $item->quality = $item->quality - 1;
+        }
+        $item->sell_in = $item->sell_in - 1;
+    }
+    
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -49,119 +49,119 @@ class GildedRoseTest extends TestCase
         $this->assertSame(0, $items[0]->quality);
     }
 
-    public function testAgedBrie(): void
-    {
-        $items = [new Item('Aged Brie', 50, 50)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Aged Brie', $items[0]->name);
-        $this->assertSame(49, $items[0]->sell_in);
-        $this->assertSame(50, $items[0]->quality);
+    // public function testAgedBrie(): void
+    // {
+    //     $items = [new Item('Aged Brie', 50, 50)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Aged Brie', $items[0]->name);
+    //     $this->assertSame(49, $items[0]->sell_in);
+    //     $this->assertSame(50, $items[0]->quality);
 
-        $items = [new Item('Aged Brie', 10, 10)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Aged Brie', $items[0]->name);
-        $this->assertSame(9, $items[0]->sell_in);
-        $this->assertSame(11, $items[0]->quality);
+    //     $items = [new Item('Aged Brie', 10, 10)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Aged Brie', $items[0]->name);
+    //     $this->assertSame(9, $items[0]->sell_in);
+    //     $this->assertSame(11, $items[0]->quality);
 
-        $items = [new Item('Aged Brie', 5, 5)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Aged Brie', $items[0]->name);
-        $this->assertSame(4, $items[0]->sell_in);
-        $this->assertSame(6, $items[0]->quality);
+    //     $items = [new Item('Aged Brie', 5, 5)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Aged Brie', $items[0]->name);
+    //     $this->assertSame(4, $items[0]->sell_in);
+    //     $this->assertSame(6, $items[0]->quality);
 
-        $items = [new Item('Aged Brie', 1, 1)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Aged Brie', $items[0]->name);
-        $this->assertSame(0, $items[0]->sell_in);
-        $this->assertSame(2, $items[0]->quality);
+    //     $items = [new Item('Aged Brie', 1, 1)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Aged Brie', $items[0]->name);
+    //     $this->assertSame(0, $items[0]->sell_in);
+    //     $this->assertSame(2, $items[0]->quality);
 
-        $items = [new Item('Aged Brie', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Aged Brie', $items[0]->name);
-        $this->assertSame(-1, $items[0]->sell_in);
-        $this->assertSame(2, $items[0]->quality); //なぜ2増えるのかは謎
-    }
+    //     $items = [new Item('Aged Brie', 0, 0)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Aged Brie', $items[0]->name);
+    //     $this->assertSame(-1, $items[0]->sell_in);
+    //     $this->assertSame(2, $items[0]->quality); //なぜ2増えるのかは謎
+    // }
 
-    public function testSulfuras(): void
-    {
-        $items = [new Item('Sulfuras, Hand of Ragnaros', 50, 50)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
-        $this->assertSame(50, $items[0]->sell_in);
-        $this->assertSame(50, $items[0]->quality);
+    // public function testSulfuras(): void
+    // {
+    //     $items = [new Item('Sulfuras, Hand of Ragnaros', 50, 50)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
+    //     $this->assertSame(50, $items[0]->sell_in);
+    //     $this->assertSame(50, $items[0]->quality);
 
-        $items = [new Item('Sulfuras, Hand of Ragnaros', 10, 10)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
-        $this->assertSame(10, $items[0]->sell_in);
-        $this->assertSame(10, $items[0]->quality);
+    //     $items = [new Item('Sulfuras, Hand of Ragnaros', 10, 10)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
+    //     $this->assertSame(10, $items[0]->sell_in);
+    //     $this->assertSame(10, $items[0]->quality);
 
-        $items = [new Item('Sulfuras, Hand of Ragnaros', 5, 5)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
-        $this->assertSame(5, $items[0]->sell_in);
-        $this->assertSame(5, $items[0]->quality);
+    //     $items = [new Item('Sulfuras, Hand of Ragnaros', 5, 5)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
+    //     $this->assertSame(5, $items[0]->sell_in);
+    //     $this->assertSame(5, $items[0]->quality);
 
-        $items = [new Item('Sulfuras, Hand of Ragnaros', 1, 1)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
-        $this->assertSame(1, $items[0]->sell_in);
-        $this->assertSame(1, $items[0]->quality);
+    //     $items = [new Item('Sulfuras, Hand of Ragnaros', 1, 1)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
+    //     $this->assertSame(1, $items[0]->sell_in);
+    //     $this->assertSame(1, $items[0]->quality);
 
-        $items = [new Item('Sulfuras, Hand of Ragnaros', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
-        $this->assertSame(0, $items[0]->sell_in);
-        $this->assertSame(0, $items[0]->quality);
-    }
+    //     $items = [new Item('Sulfuras, Hand of Ragnaros', 0, 0)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
+    //     $this->assertSame(0, $items[0]->sell_in);
+    //     $this->assertSame(0, $items[0]->quality);
+    // }
 
-    public function testBackstagePasses(): void
-    {
-        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 50, 50)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
-        $this->assertSame(49, $items[0]->sell_in);
-        $this->assertSame(50, $items[0]->quality);
+    // public function testBackstagePasses(): void
+    // {
+    //     $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 50, 50)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
+    //     $this->assertSame(49, $items[0]->sell_in);
+    //     $this->assertSame(50, $items[0]->quality);
 
-        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
-        $this->assertSame(9, $items[0]->sell_in);
-        $this->assertSame(12, $items[0]->quality);
+    //     $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
+    //     $this->assertSame(9, $items[0]->sell_in);
+    //     $this->assertSame(12, $items[0]->quality);
 
-        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 5, 5)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
-        $this->assertSame(4, $items[0]->sell_in);
-        $this->assertSame(8, $items[0]->quality);
+    //     $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 5, 5)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
+    //     $this->assertSame(4, $items[0]->sell_in);
+    //     $this->assertSame(8, $items[0]->quality);
 
-        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 1, 1)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
-        $this->assertSame(0, $items[0]->sell_in);
-        $this->assertSame(4, $items[0]->quality);
+    //     $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 1, 1)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
+    //     $this->assertSame(0, $items[0]->sell_in);
+    //     $this->assertSame(4, $items[0]->quality);
 
-        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
-        $this->assertSame(-1, $items[0]->sell_in);
-        $this->assertSame(0, $items[0]->quality);
-    }
+    //     $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('Backstage passes to a TAFKAL80ETC concert', $items[0]->name);
+    //     $this->assertSame(-1, $items[0]->sell_in);
+    //     $this->assertSame(0, $items[0]->quality);
+    // }
 
     // public function testApproveArray()
     // {
